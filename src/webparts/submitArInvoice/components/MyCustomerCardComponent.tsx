b2d75b8a61ba841287f43484819da5657a03b9b8
Steven-Chorkawy <sc13@clarington.net>
2020-07-17 15:28:20 -0400

    changed label on customer card

--- src/webparts/submitArInvoice/components/MyCustomerCardComponent.tsx
+++ src/webparts/submitArInvoice/components/MyCustomerCardComponent.tsx
@@ -32,7 +32,7 @@ export class MyCustomerCardComponent extends React.Component<IMyCustomerProps, I
       return (
         <div>
           <div>
-            Please Enter Any Customer Details. <b>* This value does not save to SharePoint yet. I'm going this for functionality.</b>
+            Enter Additional Customer Details.
           </div>
           <textarea
             className={'k-textarea k-autofill'}
