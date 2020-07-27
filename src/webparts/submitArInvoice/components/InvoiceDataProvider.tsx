8ed91844d5f77f59945e75760dce12d2e1975310
Steven Chorkawy <schorkawy@clarington.net>
2020-07-26 22:00:32 -0400

    Loading invoice docs and requests together

--- src/webparts/submitArInvoice/components/InvoiceDataProvider.tsx
+++ src/webparts/submitArInvoice/components/InvoiceDataProvider.tsx
@@ -14,6 +14,7 @@ import "@pnp/sp/fields";
 import "@pnp/sp/site-users/web";
 import { IFile } from '@pnp/sp/files';
 import { MyLists } from './enums/MyLists';
+import { filter } from '@progress/kendo-data-query/dist/npm/transducers';
 
 /** End PnP Imports */
 
@@ -31,6 +32,27 @@ interface IInvoiceDataProviderProps {
   currentUserDataState: any;
   onCurrentUserDataReceived: any;
 }
+
+
+/***********************************
+ *
+ * 0 = G/L Accounts.
+ * 1 = Approval Responses.
+ * 2 = Related Attachments.
+ * 3 = Files from RelatedAttachments.
+ *      This is used to get the URL to the files.
+ * 4 = Cancel Requests.
+ * 5 = AR Invoice Documents.
+ *
+ ***********************************/
+enum ARLoadQuery {
+  GLAccounts = 0,
+  ApprovalResponses = 1,
+  RelatedAttachments = 2,
+  FilesRelatedAttachments = 3,
+  CancelRequests = 4,
+  ARInvoiceDocuments = 5,
+};
 
 class LoadingPanel extends React.Component {
   public render() {
@@ -73,7 +95,7 @@ class InvoiceDataProvider extends React.Component<IInvoiceDataProviderProps, any
       .then(async response => {
         this.lastSuccess = this.pending;
         this.pending = '';
-        
+
         let filteredResponse = filterBy(response, this.props.filterState);
 
         // Apply Kendo grids filters.
@@ -83,7 +105,8 @@ class InvoiceDataProvider extends React.Component<IInvoiceDataProviderProps, any
         var invoiceIds = [];                // filter for accounts
         var idsForApproval = [];            // filter for approval requests. 
         var idsForRelatedAttachments = [];  // filter for related attachments. 
-        var idsForCancelRequests = [];      // filter for cancel requests. 
+        var idsForCancelRequests = [];      // filter for cancel requests.
+        var idsForARDocuments = [];
 
         // Iterate through processedResponse instead of response because if you don't this will generate a URL that over
         // 2000 characters long.
@@ -94,6 +117,8 @@ class InvoiceDataProvider extends React.Component<IInvoiceDataProviderProps, any
           idsForApproval.push(`InvoiceID eq '${element.ID}'`);
           idsForRelatedAttachments.push(`AR_x0020_Invoice_x0020_Request/ID eq ${element.ID}`);
           idsForCancelRequests.push(`AR_x0020_Invoice_x0020_Request/ID eq ${element.ID}`);
+
+          idsForARDocuments.push(`AR_x0020_RequestId eq ${element.ID}`);
 
           processedResponse.data[index].Date = new Date(processedResponse.data[index].Date);
           processedResponse.data[index].Created = new Date(processedResponse.data[index].Created);
@@ -113,16 +138,20 @@ class InvoiceDataProvider extends React.Component<IInvoiceDataProviderProps, any
             .filter(idsForRelatedAttachments.join(' or '))
             .getAll(),
           //TODO: How can I filter these results? I don't need every file.
-          sp.web.getFolderByServerRelativePath("RelatedInvoiceAttachments")
+          sp.web.getFolderByServerRelativePath(MyLists["Related Invoice Attachments"])
             .files(),
-          sp.web.lists.getByTitle('Cancel Invoice Request')
+          sp.web.lists.getByTitle(MyLists["Cancel Invoice Request"])
             .items
             .select('*, Requested_x0020_By/EMail, Requested_x0020_By/Title')
             .expand('Requested_x0020_By')
             .filter(idsForCancelRequests.join(' or '))
-            .getAll()
+            .getAll(),
+          sp.web.lists.getByTitle(MyLists["AR Invoices"])
+            .items
+            .filter(idsForARDocuments.join(' or '))
+            .getAll(),
         ])
-          .then((values) => {            
+          .then((values) => {
             /***********************************
              *
              * 0 = G/L Accounts.
@@ -131,24 +160,54 @@ class InvoiceDataProvider extends React.Component<IInvoiceDataProviderProps, any
              * 3 = Files from RelatedAttachments.
              *      This is used to get the URL to the files.
              * 4 = Cancel Requests.
+             * 5 = AR Invoice Documents.
              *
              ***********************************/
             // Using each of the accounts that we found we will not attach them to the invoice object.
-            response.map(invoice => {
-              invoice.AccountDetails = values[0].filter(f => Number(f.AR_x0020_Invoice_x0020_RequestId) === invoice.ID) || [];
-              invoice.Approvals = values[1].filter(f => Number(f.InvoiceID) === invoice.ID) || [];
-              invoice.RelatedAttachments = values[2].filter(f => Number(f.AR_x0020_Invoice_x0020_RequestId) === invoice.ID) || [];
-              invoice.CancelRequests = values[4].filter(f => Number(f.AR_x0020_Invoice_x0020_RequestId) === invoice.ID) || [];
+            for (let index = 0; index < processedResponse.data.length; index++) {
+              processedResponse.data[index];
+              
+
+              if (values[ARLoadQuery.ARInvoiceDocuments].filter(f => Number(f.AR_x0020_RequestId) === processedResponse.data[index].ID).length > 0) {
+                debugger;
+                processedResponse.data[index] = values[ARLoadQuery.ARInvoiceDocuments].filter(f => Number(f.AR_x0020_RequestId) === processedResponse.data[index].ID)[0];
+              }
+
+              processedResponse.data[index].AccountDetails = values[ARLoadQuery.GLAccounts].filter(f => Number(f.AR_x0020_Invoice_x0020_RequestId) === processedResponse.data[index].ID) || [];
+              processedResponse.data[index].Approvals = values[ARLoadQuery.ApprovalResponses].filter(f => Number(f.InvoiceID) === processedResponse.data[index].ID) || [];
+              processedResponse.data[index].RelatedAttachments = values[ARLoadQuery.RelatedAttachments].filter(f => Number(f.AR_x0020_Invoice_x0020_RequestId) === processedResponse.data[index].ID) || [];
+              processedResponse.data[index].CancelRequests = values[ARLoadQuery.CancelRequests].filter(f => Number(f.AR_x0020_Invoice_x0020_RequestId) === processedResponse.data[index].ID) || [];
 
               // Add ServerDirectUrl if required.
-              invoice.RelatedAttachments.map(relatedAttachments => {
+              processedResponse.data[index].RelatedAttachments.map(relatedAttachments => {
                 if (relatedAttachments.ServerRedirectedEmbedUrl === "") {
                   var url = values[3].find(f => f.Title === relatedAttachments.Title).ServerRelativeUrl;
                   relatedAttachments.ServerRedirectedEmbedUrl = url;
                   relatedAttachments.ServerRedirectedEmbedUri = url;
                 }
               });
-            });
+            }
+            // response.map(invoice => {
+            //   if (values[ARLoadQuery.ARInvoiceDocuments].filter(f => Number(f.AR_x0020_RequestId) === invoice.ID).length > 0) {
+            //     debugger;
+            //     invoice = values[ARLoadQuery.ARInvoiceDocuments].filter(f => Number(f.AR_x0020_RequestId) === invoice.ID)[0];
+            //   }
+
+            //   invoice.AccountDetails = values[ARLoadQuery.GLAccounts].filter(f => Number(f.AR_x0020_Invoice_x0020_RequestId) === invoice.ID) || [];
+            //   invoice.Approvals = values[ARLoadQuery.ApprovalResponses].filter(f => Number(f.InvoiceID) === invoice.ID) || [];
+            //   invoice.RelatedAttachments = values[ARLoadQuery.RelatedAttachments].filter(f => Number(f.AR_x0020_Invoice_x0020_RequestId) === invoice.ID) || [];
+            //   invoice.CancelRequests = values[ARLoadQuery.CancelRequests].filter(f => Number(f.AR_x0020_Invoice_x0020_RequestId) === invoice.ID) || [];
+
+            //   // Add ServerDirectUrl if required.
+            //   invoice.RelatedAttachments.map(relatedAttachments => {
+            //     if (relatedAttachments.ServerRedirectedEmbedUrl === "") {
+            //       var url = values[3].find(f => f.Title === relatedAttachments.Title).ServerRelativeUrl;
+            //       relatedAttachments.ServerRedirectedEmbedUrl = url;
+            //       relatedAttachments.ServerRedirectedEmbedUri = url;
+            //     }
+            //   });
+            // });
+            debugger;
 
             // This is something from Kendo demos.
             if (toODataString(this.props.dataState) === this.lastSuccess) {
@@ -184,12 +243,12 @@ class InvoiceDataProvider extends React.Component<IInvoiceDataProviderProps, any
         this.lastSuccess = this.pending;
         this.pending = '';
 
-        
+
         let filteredResponse = filterBy(response, this.props.filterState);
 
         // Apply Kendo grids filters.
         var processedResponse = process(filteredResponse, this.props.dataState);
-        
+
         // Hold the list of invoice IDs that will be used to pull related accounts.
         var invoiceIds = [];
         var idsForApproval = [];
@@ -267,7 +326,7 @@ class InvoiceDataProvider extends React.Component<IInvoiceDataProviderProps, any
 
             // This is something from Kendo demos.
             if (toODataString(this.props.dataState) === this.lastSuccess) {
-              
+
               this.props.onDataReceived.call(undefined, {
                 // Add the filtered, sorted data.
                 data: processedResponse.data,
