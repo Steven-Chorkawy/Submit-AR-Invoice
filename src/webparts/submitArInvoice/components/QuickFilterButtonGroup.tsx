f3288b63d2637ba0ba32626a6bb88d567666f45d
Steven-Chorkawy <sc13@clarington.net>
2020-08-28 16:01:42 -0400

    HOW TO GET CURRENT USER?!?!?!

--- src/webparts/submitArInvoice/components/QuickFilterButtonGroup.tsx
+++ src/webparts/submitArInvoice/components/QuickFilterButtonGroup.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
 
+// Import PnP stuff.
+import { sp } from "@pnp/sp";
+import "@pnp/sp/webs";
+import "@pnp/sp/site-users/web";
+
 // Import Kendo Components
 import { Button, ButtonGroup } from '@progress/kendo-react-buttons';
 
@@ -18,21 +23,45 @@ class QuickFilterButtonGroup extends React.Component<IQuickFilterButtonGroupProp
     super(props);
   }
 
+  private _getCurrentUser = () => {
+    return sp.web.currentUser.get()
+      .then(user => { return user; });
+  };
+
   private _submittedInvoices = () => {
     return this.props.invoices.filter(f => f.Invoice_x0020_Status === InvoiceStatus.Submitted);
   }
 
+  // Get invoices that have actions assigned to this user with a status of Waiting.
   private _invoicesForCurrentUser = () => {
-    //TODO: Write filter for invoices that have an action that needs the current users approval.
+    Promise.all([sp.web.currentUser])
+      .then(res => {
+        res[0].Id
+      });
+
+    return this.props.invoices.filter(x =>
+      x.Actions.some(y =>
+        y.Response_x0020_Status === InvoiceActionResponseStatus.Waiting
+        && y.AssignedToId === this._getCurrentUser().Id
+      )
+    );
   }
 
+  // return invoices that have all actions with a status of approved.
   private _approvedInvoices = () => {
     return this.props.invoices
-      .filter(f =>
-        f.Actions.filter(ff =>
-          ff.Response_x0020_Status === InvoiceActionResponseStatus.Approved
-        ).length === f.Actions.length
+      .filter(
+        f => f.Actions.filter(ff => ff.Response_x0020_Status === InvoiceActionResponseStatus.Approved)
+          .length === f.Actions.length && f.Actions.length > 0
       );
+  }
+
+  private _waitingApproval = () => {
+    return this.props.invoices.filter(x =>
+      x.Actions.some(y =>
+        y.Response_x0020_Status === InvoiceActionResponseStatus.Waiting
+      )
+    );
   }
 
   public render() {
@@ -43,16 +72,16 @@ class QuickFilterButtonGroup extends React.Component<IQuickFilterButtonGroupProp
             Show All ({this.props.invoices.length})
           </Button>
           <Button>
-            For You (0)
+            For You ({this._invoicesForCurrentUser().length})
           </Button>
           <Button onClick={e => { this.props.onButtonClick(e, this._submittedInvoices()); }}>
             Submitted ({this._submittedInvoices().length})
           </Button>
-          <Button>
-            Approved (0)
+          <Button onClick={e => { this.props.onButtonClick(e, this._approvedInvoices()); }}>
+            Approved ({this._approvedInvoices().length})
           </Button>
-          <Button>
-            Waiting Approval (0)
+          <Button onClick={e => { this.props.onButtonClick(e, this._waitingApproval()); }}>
+            Waiting Approval ({this._waitingApproval.length})
           </Button>
         </ButtonGroup>
       </div>
