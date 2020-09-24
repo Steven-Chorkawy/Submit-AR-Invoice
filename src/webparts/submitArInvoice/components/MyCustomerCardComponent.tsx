9d55b53e0eaff082ad7d0dcc5f982274d34687c0
Steven-Chorkawy <sc13@clarington.net>
2020-09-24 11:07:40 -0400

    Adding missing semicolon.

--- src/webparts/submitArInvoice/components/MyCustomerCardComponent.tsx
+++ src/webparts/submitArInvoice/components/MyCustomerCardComponent.tsx
@@ -64,7 +64,7 @@ export class MyCustomerCardComponent extends React.Component<any, any> {
               onClick={(e) => {
                 this.setState({
                   showMore: !this.state.showMore
-                })
+                });
               }}
             >
               {
