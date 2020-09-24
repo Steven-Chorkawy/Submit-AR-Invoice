e183fd6f3eceebac5732df86ac5232e3b81f147e
Steven-Chorkawy <sc13@clarington.net>
2020-09-24 09:50:08 -0400

    Hiding title of column, removing Request type from grids.

--- src/webparts/submitArInvoice/components/DepartmentForm/MyKendoGrid.tsx
+++ src/webparts/submitArInvoice/components/DepartmentForm/MyKendoGrid.tsx
@@ -589,13 +589,13 @@ export class MyKendoGrid extends React.Component<any, MyKendoGridState> {
             />
           </GridToolbar>
 
-          <Column width="75px" field="FileRef" title="" filterable={false} sortable={false} cell={this.MyCustomCell} />
+          <Column width="75px" field="" title="" filterable={false} sortable={false} cell={this.MyCustomCell} />
           <Column field="ID" title="ID" width="75px" filterable={false} />
           <Column field="Created" width="250px" title="Created Date" filter="date" format={MyGridStrings.DateFilter} />
           <Column field="Customer.Customer_x0020_Name" width="250px" title="Customer" />
           <Column field="Invoice_x0020_Status" width="250px" title="Status" />
           <Column field="Date" title="Date" width="250px" filter="date" format={MyGridStrings.DateFilter} />
-          <Column field="Type_x0020_of_x0020_Request" width="250px" title="Type" />
+          {/* <Column field="Type_x0020_of_x0020_Request" width="250px" title="Type" /> */}
 
           <Column cell={this.CommandCell} width={"110px"} locked={true} resizable={false} filterable={false} sortable={false} />
 
