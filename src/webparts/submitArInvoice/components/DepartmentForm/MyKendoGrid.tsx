8f8e2d717ddd22a0c3ab0b7e9f767d88834ef744
Steven-Chorkawy <sc13@clarington.net>
2020-09-24 10:12:25 -0400

    Created a custom ID cell that will indicate if an invoice is urgent or not.

--- src/webparts/submitArInvoice/components/DepartmentForm/MyKendoGrid.tsx
+++ src/webparts/submitArInvoice/components/DepartmentForm/MyKendoGrid.tsx
@@ -32,6 +32,7 @@ import { InvoiceGridDetailComponent } from '../InvoiceGridDetailComponent';
 import { MyLists } from '../enums/MyLists';
 import { MyContentTypes } from '../enums/MyEnums';
 import { FileRefCell } from '../FileRefCell';
+import { IDCell } from '../IDCell';
 import { IInvoiceItem, IPersonField, IInvoiceUpdateItem } from '../interface/InvoiceItem';
 import { IMySaveResult } from '../interface/IMySaveResult';
 import { QuickFilterButtonGroup } from '../QuickFilterButtonGroup';
@@ -590,7 +591,7 @@ export class MyKendoGrid extends React.Component<any, MyKendoGridState> {
           </GridToolbar>
 
           <Column width="75px" field="" title="" filterable={false} sortable={false} cell={this.MyCustomCell} />
-          <Column field="ID" title="ID" width="75px" filterable={false} />
+          <Column field="ID" title="ID" width="75px" filterable={false} cell={(props) => <IDCell {...props} />} />
           <Column field="Created" width="250px" title="Created Date" filter="date" format={MyGridStrings.DateFilter} />
           <Column field="Customer.Customer_x0020_Name" width="250px" title="Customer" />
           <Column field="Invoice_x0020_Status" width="250px" title="Status" />
