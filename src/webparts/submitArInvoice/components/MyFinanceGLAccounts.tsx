fcb878b42db3f6a4d895bdcd5f4c2442e9f4c561
Admin <scadmin@clarington.net>
2020-11-12 11:40:23 -0500

    Adding list view. Next is to add the save events.

--- src/webparts/submitArInvoice/components/MyFinanceGLAccounts.tsx
+++ src/webparts/submitArInvoice/components/MyFinanceGLAccounts.tsx
@@ -13,9 +13,12 @@ import "@pnp/sp/items";
 
 
 import { Field } from '@progress/kendo-react-form';
-import { NumericTextBox, Checkbox } from '@progress/kendo-react-inputs';
+import { NumericTextBox, Checkbox, MaskedTextBox } from '@progress/kendo-react-inputs';
 import { Grid, GridColumn, GridToolbar } from '@progress/kendo-react-grid';
 import { Button } from '@progress/kendo-react-buttons';
+import { Card, CardTitle, CardSubtitle, CardBody } from '@progress/kendo-react-layout';
+import { ListView, ListViewHeader } from '@progress/kendo-react-listview';
+import { DropDownList } from '@progress/kendo-react-dropdowns';
 
 import * as MyValidators from './validators.jsx';
 import * as MyFormComponents from './MyFormComponents';
@@ -36,7 +39,6 @@ import { IARInvoiceAccount } from './interface/IARInvoiceAccount';
 const CalculateHSTAmount = (props) => {
   return (props.dataItem.HSTTaxable == true) ? props.dataItem.Amount * 0.13 : 0;
 };
-
 
 const glCodeCell = (props) => {
   const { dataItem, field } = props;
@@ -70,8 +72,6 @@ const glCodeCell = (props) => {
     </td>
   );
 };
-
-
 
 /**
  * Amount before HST.
@@ -480,6 +480,112 @@ export class MyFinanceGlAccounts extends React.Component<any, any> {
   }
 }
 
+class GLAccountsListViewItemRender extends React.Component<any, any> {
+  public state = {
+    item: this.props.dataItem
+  };
+  public componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.dataItem.ProductID !== this.props.dataItem.ProductID) {
+      this.setState({
+        item: this.props.dataItem
+      });
+    }
+  }
+  public enterEdit = () => {
+    this.setState({ item: { ...this.state.item, edit: true } });
+  }
+  public cancelEdit = () => {
+    this.setState({ item: { ...this.state.item, edit: false } });
+  }
+  public handleChange = (e, field) => {
+    let updatedItem = { ...this.state.item };
+    updatedItem[field] = e.value;
+    this.setState({ item: updatedItem });
+  }
+  public handleSave = () => {
+    this.props.saveItem(this.state.item);
+    this.setState({ item: { ...this.state.item, edit: false } });
+  }
+  public handleDelete = () => {
+    this.props.deleteItem(this.state.item);
+  }
+  public render() {
+    const item = this.props.dataItem;
+    return (
+      <div key={this.props.dataItem.ProductID}>
+        <Card orientation='horizontal' style={{ borderWidth: '0px 0px 1px' }}>
+          {
+            this.state.item.edit ?
+              <CardBody>
+                <div className={'row'}>
+                  <div className={'col-md-10'}>
+                    <div className={'row'}>
+                      <div className={'col-md-6'}>
+                        <label style={{ display: 'block' }}>Account Code:</label>
+                        <MaskedTextBox
+                          id={'Account_x0020_Code'}
+                          name={'Account_x0020_Code'}
+                          mask="000-00-000-00000-0000"
+                          value={this.state.item.Account_x0020_Code}
+                          onChange={(e) => this.handleChange(e, 'Account_x0020_Code')}
+                        />
+                      </div>
+                      <div className={'col-md-6'}>
+                        <label style={{ display: 'block' }}>Amount:</label>
+                        <NumericTextBox value={this.state.item.Amount} format="c2" min={0} onChange={(e) => this.handleChange(e, 'Amount')} />
+                      </div>
+                    </div>
+                    <div className={'row'}>
+                      <div className={'col-md-6'}>
+                        <div className={'col-md-6'}>
+                          <label style={{ display: 'block' }}>HST:</label>
+                          <Checkbox label={'HST'} value={this.state.item.HST_x0020_Taxable} onChange={(e) => this.handleChange(e, 'HST_x0020_Taxable')} />
+                        </div>
+                        <div className={'col-md-6'}>
+                          <label style={{ display: 'block' }}>HST Amount:</label>
+                          <NumericTextBox value={this.state.item.HST_x0020_Taxable ? this.state.item.Amount * 0.13 : 0} format="c2" disabled={true} min={0} />
+                        </div>
+                      </div>
+                      <div className={'col-md-6'}>
+                        <label style={{ display: 'block' }}>Total:</label>
+                        <NumericTextBox value={this.state.item.HST_x0020_Taxable ? (this.state.item.Amount * 0.13) + this.state.item.Amount : this.state.item.Amount} disabled={true} format="c2" min={0} />
+                      </div>
+                    </div>
+                  </div>
+                  <div className={'col-md-2'}>
+                    <Button primary={true} look={'flat'} title={'Save'} icon={'save'} style={{ marginRight: 5 }} onClick={this.handleSave}></Button>
+                    <Button icon={'cancel'} look={'flat'} title={'Cancel'} onClick={this.cancelEdit}></Button>
+                  </div>
+                </div>
+              </CardBody>
+              : <CardBody>
+                <div className={'row'}>
+                  <div className={'col-md-10'}>
+                    <div className={'row'}>
+                      <div className={'col-md-6'}>
+                        <p>
+                          {item.Account_x0020_Code}
+                        </p>
+                      </div>
+                      <div className={'col-md-6'}>
+                        <div>Amount: {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(item.Amount)}</div>
+                        <div>Total: {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(item.Total_x0020_Invoice)}</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className={'col-md-2'}>
+                    <Button primary={true} look={'flat'} title={'Edit'} icon={'edit'} style={{ marginRight: 5 }} onClick={this.enterEdit}></Button>
+                    <Button icon={'trash'} look={'flat'} title={'Delete'} onClick={this.handleDelete}></Button>
+                  </div>
+                </div>
+              </CardBody>
+          }
+        </Card>
+      </div>
+    );
+  }
+}
+
 export class GLAccountsListView extends React.Component<any, any> {
 
   constructor(props) {
@@ -487,18 +593,39 @@ export class GLAccountsListView extends React.Component<any, any> {
     debugger;
   }
 
+  public state = {
+    value: this.props.value
+  };
+
+  public MyCustomItem = props => <GLAccountsListViewItemRender
+    {...props}
+    saveItem={(e) => { console.log('saveItem'); console.log(e); }}
+    deleteItem={(e) => { console.log('deleteItem'); console.log(e); }}
+  />;
+
+  public MyHeader = () => {
+    return (
+      <ListViewHeader style={{ color: 'rgb(160, 160, 160)', fontSize: 14 }} className='pl-3 pb-2 pt-2'>
+        <Button primary={true} icon={'plus'} onClick={(e) => {
+          this.setState({
+            value: [...this.state.value, { edit: true }]
+          });
+        }}>Add New Account</Button>
+      </ListViewHeader>
+    );
+  }
+
   /**
    * render
    */
   public render() {
     return (
-      <div>hello</div>
-      // <ListView
-      //           data={this.props.productInEdit.}
-      //           item={this.MyCustomItem}
-      //           style={{ width: "100%", height: 500 }}
-      //           header={MyHeader}
-      //       />
+      <ListView
+        data={this.state.value}
+        item={this.MyCustomItem}
+        style={{ width: "100%" }}
+        header={this.MyHeader}
+      />
     );
   }
 }
@@ -522,9 +649,9 @@ export const MyFinanceGlAccountsComponent = (fieldArrayRenderProps) => {
 };
 
 export const GLAccountsListViewComponent = (fieldArrayRenderProps) => {
-  return(
+  return (
     <div key={fieldArrayRenderProps.value}>
       <GLAccountsListView {...fieldArrayRenderProps} />
     </div>
   );
-}
+};
