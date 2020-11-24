da20ebd7b979b27ad597a271c6fc5d9162fe7301
Admin <scadmin@clarington.net>
2020-11-24 14:05:24 -0500

    Adding method to get all user properties.

--- src/webparts/submitArInvoice/components/MyHelperMethods.tsx
+++ src/webparts/submitArInvoice/components/MyHelperMethods.tsx
@@ -141,3 +141,22 @@ export const SendApprovalResponse = async (response: string, comment: string, in
       });
   }
 };
+
+/**
+ * Get user profile details.
+ * @param loginName A Users LoginName
+ * @param callBack Call Back method is passed the users profile.
+ */
+export const GetUserProfile = async (loginName: string, callBack: Function) => {
+  sp.profiles.getPropertiesFor(loginName).then(userProfileRes => {
+    // This converts UserProfileProperties from an array of key value pairs [{Key:'', Value: ''},{Key:'', Value: ''}]
+    // Into an array of objects [{'Key': 'Value'}, {'Key: 'Value'}]
+    let props = {};
+    userProfileRes.UserProfileProperties.map(p => {
+      props[p.Key] = p.Value;
+    });
+    userProfileRes['Props'] = { ...props };
+
+    callBack(userProfileRes);
+  });
+};
