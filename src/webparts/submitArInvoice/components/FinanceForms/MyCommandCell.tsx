9dc44ee9622341c019757ebddc9586aac82cf8d0
Steven-Chorkawy <65408672+Steven-Chorkawy@users.noreply.github.com>
2020-12-09 12:10:44 -0500

    Adding the approval (#99)
    * Adding the approval
    
    * Updating version number.
    
    Co-authored-by: Admin <scadmin@clarington.net>

--- src/webparts/submitArInvoice/components/FinanceForms/MyCommandCell.tsx
+++ src/webparts/submitArInvoice/components/FinanceForms/MyCommandCell.tsx
@@ -3,13 +3,16 @@ import * as React from 'react';
 import { GridCell } from '@progress/kendo-react-grid';
 
 import { Button } from '@progress/kendo-react-buttons';
+import { InvoiceActionResponseStatus } from '../enums/MyEnums';
 
-export function MyCommandCell({ edit, remove, add, update, discard, cancel, editField }) {
+export function MyCommandCell({ edit, remove, add, update, discard, cancel, editField, currentUser, approvalResponse }) {
   return class extends GridCell {
     public render() {
       const { dataItem } = this.props;
       const inEdit = dataItem[editField];
       const isNewItem = dataItem.ID === undefined;
+      const needsApproval: Boolean = dataItem.Actions.some(y => y.Response_x0020_Status === InvoiceActionResponseStatus.Waiting && y.AssignedToId === currentUser.Id);
+
 
       return inEdit ? (
         <td className={this.props.className + " k-command-cell row row-no-gutters"} style={this.props.style}>
@@ -31,11 +34,21 @@ export function MyCommandCell({ edit, remove, add, update, discard, cancel, edit
       ) : (
           <td className={this.props.className + " k-command-cell"} style={this.props.style}>
             <Button
-              className="k-primary k-button k-grid-edit-command col-sm-12"
+              className="k-grid-edit-command col-sm-12"
               onClick={() => edit(dataItem)}
               icon="edit"
+              look='flat'
               style={{ "marginBottom": "5px" }}
             >Edit</Button>
+            {
+              needsApproval &&
+              <Button
+                primary={true}
+                className="k-grid-edit-command col-sm-12"
+                onClick={() => approvalResponse(dataItem)}
+                style={{ "marginBottom": "5px" }}
+              >Approve/Deny</Button>
+            }
           </td>
         );
     }
