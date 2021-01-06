a77349825818968440b726b5333f1fcae7cfaee7
Admin <scadmin@clarington.net>
2021-01-06 14:55:22 -0500

    Making a new component file to replace the old one.

---
+++ src/webparts/submitArInvoice/components/CreateARInvoiceForm.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+
+// Kendo Imports
+import { Form, Field, FormElement, FieldWrapper, FieldArray } from '@progress/kendo-react-form';
+import { Button } from '@progress/kendo-react-buttons';
+import { Card, CardTitle, CardBody, CardActions } from '@progress/kendo-react-layout';
+import { filterBy } from '@progress/kendo-data-query';
+import { Label } from '@progress/kendo-react-labels';
+
+// PNP Imports
+import { sp } from "@pnp/sp";
+import { Web } from "@pnp/sp/webs";
+import "@pnp/sp/webs";
+import "@pnp/sp/files";
+import "@pnp/sp/folders";
+import "@pnp/sp/lists";
+import "@pnp/sp/items";
+import "@pnp/sp/profiles";
+
+// Office UI Imports
+import { Persona, PersonaSize } from 'office-ui-fabric-react/lib/Persona';
+import { Shimmer } from 'office-ui-fabric-react/lib/Shimmer';
+
+// My custom imports
+import * as MyFormComponents from './MyFormComponents';
+import { IUploadingFile } from './IMyFormState';
+import * as MyValidators from './validators.jsx';
+import { MyGLAccountComponent } from './MyGLAccountComponent';
+
+import { BuildGUID, GetUserByEmail, GetUserById, GetUserByLoginName, GetUsersByLoginName, GetUserProfile, GetDepartments } from './MyHelperMethods';
+
+import './PersonaComponent';
+
+export interface IMyFormProps {
+  siteUsers: any;
+  customerList: any;
+  context: any;
+}
+
+export class SubmitARInvoiceForm extends React.Component<IMyFormProps, any> {
+
+  constructor(props) {
+    super(props);  
+  }
+
+
+  public render() {
+    return (
+      <h1>Submit Form here.</h1>
+    );
+  }
+}
+
