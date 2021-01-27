f09d0e9a5da9491c40644e31ba0f75e0db9c57c5
Admin <scadmin@clarington.net>
2021-01-27 11:24:39 -0500

    Checking if user has edit permissions on list item.  If they do not I'm disabling various components.

--- src/webparts/submitArInvoice/components/DepartmentForm/DepartmentGridEditDialogContainer.tsx
+++ src/webparts/submitArInvoice/components/DepartmentForm/DepartmentGridEditDialogContainer.tsx
@@ -19,6 +19,7 @@ import { GetDepartments } from '../MyHelperMethods';
 
 import { IInvoiceItem } from '../interface/MyInterfaces';
 import { MyLists } from '../enums/MyLists';
+import { PermissionKind } from '@pnp/sp/security';
 
 interface IDepartmentGridEditDialogContainerState {
   productInEdit: IInvoiceItem;
@@ -28,9 +29,10 @@ interface IDepartmentGridEditDialogContainerState {
   loading?: boolean;
   standardTerms: Array<any>;
   departments: any[];
+  hasEditPermissions: boolean;
 }
 
-function GridButtons({ cancel, saveResult }) {
+function GridButtons({ cancel, saveResult }, hasEditPermissions: boolean) {
   return (
     <div>
       {saveResult && saveResult.success === false &&
@@ -49,6 +51,8 @@ function GridButtons({ cancel, saveResult }) {
           style={{ width: '50%' }}
           className="k-button k-primary"
           icon="save"
+          disabled={!hasEditPermissions}
+          title={hasEditPermissions ? 'Save Changes.' : 'Cannot Save Changes. Insufficient Permissions'}
         >Save</Button>
         <Button
           style={{ width: '50%' }}
@@ -71,8 +75,18 @@ export class DepartmentGridEditDialogContainer extends React.Component<any, IDep
       customerList: this.props.customers,
       receivedCustomerList: this.props.customers,
       standardTerms: [],
-      departments: this.props.departments ? [...this.props.departments] : []
+      departments: this.props.departments ? [...this.props.departments] : [],
+      hasEditPermissions: false
     };
+
+    sp.web.lists.getByTitle(MyLists["AR Invoice Requests"]).items.getById(this.props.dataItem.ID).currentUserHasPermissions(PermissionKind.EditListItems).then(hasEditPermissions => {
+      // hasEditPermissions === true if user has edit permissions. 
+      // hasEditPermissions === false if user does not have edit permissions
+      this.setState({ hasEditPermissions: hasEditPermissions });
+    }).catch(reason => {
+      console.log(reason);
+      alert('Something went wrong.  Please contact helpdesk@clarington.net');
+    });
 
     sp.web.lists.getByTitle(MyLists["AR Invoice Requests"]).fields
       .getByInternalNameOrTitle('Standard_x0020_Terms')
@@ -130,7 +144,7 @@ export class DepartmentGridEditDialogContainer extends React.Component<any, IDep
           initialValues={{ ...this.state.productInEdit }}
           render={(formRenderProps) => (
             <FormElement>
-              {GridButtons({ cancel: this.props.cancel, saveResult: this.props.saveResult })}
+              {GridButtons({ cancel: this.props.cancel, saveResult: this.props.saveResult }, this.state.hasEditPermissions)}
               <div className={'row'}>
                 <div className={'col-sm-6'}>
                   <div style={{ display: 'block', justifyContent: 'space-between' }}>
@@ -172,7 +186,7 @@ export class DepartmentGridEditDialogContainer extends React.Component<any, IDep
                 </div>
                 <div className={'col-sm-6'}>
                   <Label>Approval Requests</Label>
-                  <ActionStepsComponent actions={this.props.dataItem.Actions} onAddNewApproval={this.props.onAddNewApproval} />
+                  <ActionStepsComponent actions={this.props.dataItem.Actions} onAddNewApproval={this.state.hasEditPermissions ? this.props.onAddNewApproval : null} />
                 </div>
               </div>
               <Field
@@ -250,7 +264,7 @@ export class DepartmentGridEditDialogContainer extends React.Component<any, IDep
                 onRemove={this.props.onRelatedAttachmentRemove}
               />
 
-              {GridButtons({ cancel: this.props.cancel, saveResult: this.props.saveResult })}
+              {GridButtons({ cancel: this.props.cancel, saveResult: this.props.saveResult }, this.state.hasEditPermissions)}
             </FormElement>
           )}
         >
